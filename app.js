3952107042f89fbb043e7f7ce6bf3d7d0c893e28
susan-chips <adesojisusan.adeola@gmail.com>
2023-05-20 23:17:55 -0500

    Add feature Midlleware

--- app.js
+++ app.js
@@ -3,19 +3,16 @@ const http = require('http');
 const express = require('express');
 
 const app = express();
-
-
-app.use((req, res, next)=>{
-    console.log('IN the middleware')
-    next();
+app.use('/add-product', (req, res, next)=>{
+    console.log('In middleware')
+    res.send('<h1>The Add product Page!</h1>');
 });
-app.use((req, res, next)=>{
+
+app.use('/', (req, res, next)=>{
     console.log('IN another middleware')
+    res.send('<h1> Hello from Express!</h1>');
 });
 
-
-const server = http.createServer(app); 
-
-server.listen(3000);
+app.listen(3000)
 
 
