9b4c4cece8d756b340a5175c84940bbad910d290
susan-chips <adesojisusan.adeola@gmail.com>
2023-05-25 19:26:11 -0500

    Added routes

--- app.js
+++ app.js
@@ -1,24 +1,17 @@
-
 const express = require ('express');
-
-const adminRoutes = require('./routes/admin.js');
-const adminRoutes = require('./routes/shop.js')
-
 const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin.js');
+const shopRoutes = require('./routes/shop.js')
+
 app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.use(bodyParser.urlencoded({extended: false}));
 
 
-app.use('/', (req, res, next)=>{
-    res.send('<h1> Hello from Express!</h1>');
-});
-app.use('/', (req, res, next)=>{
-    res.send('<h1> Hello from Express!</h1>');
-});
 
 app.listen(3000)
 
