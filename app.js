7d3bcc18556ac06373a5a68f72e3499505574f2f
susan-chips <adesojisusan.adeola@gmail.com>
2023-05-25 19:40:07 -0500

    Added views

--- app.js
+++ app.js
@@ -3,15 +3,19 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin.js');
-const shopRoutes = require('./routes/shop.js')
+const adminRoutes = require('../routes/admin.js');
+const shopRoutes = require('../routes/shop.js')
+
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(adminRoutes);
 app.use(shopRoutes);
 
-app.use(bodyParser.urlencoded({extended: false}));
 
-
+app.use((req, res, next) => {
+    res.status().send('<h1> Page Not Found </h1>');
+});
 
 app.listen(3000)
 
