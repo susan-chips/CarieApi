338b76a7690bff16453c70f2aec2ad2785ee5a4d
susan-chips <adesojisusan.adeola@gmail.com>
2023-06-07 23:26:55 -0500

    features

--- play.js
+++ play.js
@@ -1,69 +1,69 @@
-// //Functions and constants
-//  const name = 'Max';
-// let age = 23;
-// const hasHobbies = true;
+//Functions and constants
+ const name = 'Max';
+let age = 23;
+const hasHobbies = true;
 
-// age= 43;
+age= 43;
 
-// const summarizeUser = (name, age, hasHobbies) => {
-//     return (
-//         'the users name is ' + 
-//         name  +
-//         ', and the user age is ' +
-//          age  +
-//         ', in additiion, the user hobby is ' + hasHobbies
-//     );
-// };
-// console.log(summarizeUser(name, age, hasHobbies));
-
-
-//  Ojects and method, Objects allows us to group data 
-
-// const person = {
-//     name: 'susan',
-//     age: 25,
-//     greet(){
-//         console.log('Hi, my name is ' + this.name);
-//     }
-// }; 
-
-// // person.greet();
+const summarizeUser = (name, age, hasHobbies) => {
+    return (
+        'the users name is ' + 
+        name  +
+        ', and the user age is ' +
+         age  +
+        ', in additiion, the user hobby is ' + hasHobbies
+    );
+};
+console.log(summarizeUser(name, age, hasHobbies));
 
 
-// const hobbies = ['paintin', 'sleeping'];
-// // for(let hobby of hobbies){
-// //     console.log(hobby);
-// // }
+ Ojects and method, Objects allows us to group data 
 
-// // console.log(hobbies.map(hobby => 'hobbies are ' + hobby));
-// // console.log (hobbies);
+const person = {
+    name: 'susan',
+    age: 25,
+    greet(){
+        console.log('Hi, my name is ' + this.name);
+    }
+}; 
 
-// // hobbies.push('loving');
-
-// this is called the spread operator
-// const spread = {...person};
-// console.log(spread);
+// person.greet();
 
 
-//  use the rest operator to extend the arguments passed into a functions
+const hobbies = ['paintin', 'sleeping'];
+// for(let hobby of hobbies){
+//     console.log(hobby);
+// }
 
-// const restOperator = (...args) => {
-//     return args;
+// console.log(hobbies.map(hobby => 'hobbies are ' + hobby));
+// console.log (hobbies);
 
-// };
+// hobbies.push('loving');
 
-// console.log(restOperator(1,2,3,4))
+this is called the spread operator
+const spread = {...person};
+console.log(spread);
 
 
-// sychronus and asynchronos code - syn happens immdeiately while asyn as delay
+ use the rest operator to extend the arguments passed into a functions
+
+const restOperator = (...args) => {
+    return args;
+
+};
+
+console.log(restOperator(1,2,3,4))
 
 
-// setTimeout(() => {
-//     console.log('this is async code')
-// }, 40);
+sychronus and asynchronos code - syn happens immdeiately while asyn as delay
 
-// console.log('this is synchro');
-// console.log('this is code');
+
+setTimeout(() => {
+    console.log('this is async code')
+}, 40);
+
+console.log('this is synchro');
+console.log('this is code');
 
  
 
